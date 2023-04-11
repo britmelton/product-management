83be22abc1f04ed31b95b22a122a70f6f34be613
britmelton <brittany134340@gmail.com>
2023-04-11 16:12:35 -0400

    added test for Product Sku property

--- catalog-spec/Products/WhenRegisteringAProduct.cs
+++ catalog-spec/Products/WhenRegisteringAProduct.cs
@@ -8,10 +8,11 @@ namespace catalog_spec.Products
         private readonly Product _product;
         private readonly string _description = "product description";
         private readonly string _name = "product name";
+        private readonly string _sku = "123abc";
 
         public WhenRegisteringAProduct()
         {
-            _product = new Product(_description, _name);
+            _product = new Product(_description, _name, _sku);
         }
 
         [Fact]
@@ -26,5 +27,10 @@ namespace catalog_spec.Products
             _product.Name.Should().Be(_name);
         }
 
+        [Fact]
+        public void ThenSkuIsSet()
+        {
+            _product.Sku.Should().Be(_sku);
+        }
     }
 }
--- catalog/Product.cs
+++ catalog/Product.cs
@@ -4,11 +4,13 @@
     {
         public string Description { get; set; }
         public string Name { get; set; }
+        public string Sku { get; set; }
 
-        public Product(string description, string name)
+        public Product(string description, string name, string sku)
         {
             Description = description;
             Name = name;
+            Sku = sku;
         }
     }
 }
