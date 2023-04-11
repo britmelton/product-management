b8146d7158df0440bdd9295366b703413ffbe999
britmelton <brittany134340@gmail.com>
2023-04-11 16:06:26 -0400

    added test for Product Name property

---
+++ catalog-spec/Products/WhenRegisteringAProduct.cs
@@ -0,0 +1,22 @@
+﻿using catalog;
+using FluentAssertions;
+
+namespace catalog_spec.Products
+{
+    public class WhenRegisteringAProduct
+    {
+        private readonly Product _product;
+        private readonly string _name = "product name";
+
+        public WhenRegisteringAProduct()
+        {
+            _product = new Product(_name);
+        }
+
+        [Fact]
+        public void ThenNameIsSet()
+        {
+            _product.Name.Should().Be(_name);
+        }
+    }
+}
---
+++ catalog/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace catalog
+{
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public Product(string name)
+        {
+            Name = name;
+        }
+    }
+}
